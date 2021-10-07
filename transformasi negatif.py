2af003c54659d8f04ace83dadb29cc76e6c7aaa0
MuhammadThorikRifai <92023795+MuhammadThorikRifai@users.noreply.github.com>
2021-10-07 11:08:29 +0800

    Add files via upload

---
+++ transformasi negatif.py
@@ -0,0 +1,11 @@
+import cv2
+
+img = cv2.imread("thor.jpeg", 0)
+
+img_1 = 255 - img
+
+cv2.imshow("original image", img)
+cv2.imshow("image negative", img_1)
+
+cv2.waitKey(0)
+cv2.destroyAllWindows()
